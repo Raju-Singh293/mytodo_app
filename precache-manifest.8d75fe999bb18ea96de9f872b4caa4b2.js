2643bb030c481c0ea563d0420a173af8566a7b1a
Raju-Singh293 <rathorerajusingh293@gmail.com>
2022-09-07 02:15:42 +0530

    Updates

---
+++ precache-manifest.8d75fe999bb18ea96de9f872b4caa4b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edba25a82dba17b8dc0c",
+    "url": "/mytodo_app/static/css/main.e4d0750a.chunk.css"
+  },
+  {
+    "revision": "edba25a82dba17b8dc0c",
+    "url": "/mytodo_app/static/js/main.4003432a.chunk.js"
+  },
+  {
+    "revision": "e820cebe24dd7aaf544b",
+    "url": "/mytodo_app/static/js/runtime~main.5cbffe8c.js"
+  },
+  {
+    "revision": "663c505c6823b6f648cd",
+    "url": "/mytodo_app/static/js/2.56e55367.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/mytodo_app/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "d0bd08fed608bc5c0cecb6123a3ed2c8",
+    "url": "/mytodo_app/index.html"
+  }
+];
